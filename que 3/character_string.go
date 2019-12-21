df26bb9819611d92a7e82b0193868bb03c3ab37e
TripleThreads <segnih8@gmail.com>
2019-12-21 11:15:08 +0300

    count characters

--- que 3/character_string.go
+++ que 3/character_string.go
@@ -8,6 +8,15 @@ import (
 func main() {
 	name := "asSASA ddd dsjkdsjs dk"
 
+	dict := make(map[int32]int)
+
+	for _, char := range name {
+		dict[char]++
+	}
+
+	for key, value := range dict {
+		fmt.Printf("character %c appeared %d times \n", key, value)
+	}
 	fmt.Println(len(name))
 
 	count := utf8.RuneCountInString(name)
